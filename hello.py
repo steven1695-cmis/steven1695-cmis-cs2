d6b1e1b09441704eea2dbc71996107fe9fb7e4a4
cmis <cmis@cmis-hs02.(none)>
2016-01-27 11:13:38 +0700

    First looks on python

---
+++ hello.py
@@ -0,0 +1,14 @@
+print "Hello World"
+print "LOOLOLOLOL"
+print "I'm so good at programming. LLOLOL"
+print "Git gud kid"
+print "Eternal shame...."
+print False
+print True
+print 2
+print 3.1415926535
+print False
+print 03
+print ("Nathan", "Happiness", "joy", "depression", 5)
+print "Scew you mate", "You're a terrible friend"
+print "how old are you?" "5?"
