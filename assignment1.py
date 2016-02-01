de8a2f356f10242694327301ceae450b57d063d3
cmis <cmis@cmis-hs02.(none)>
2016-02-01 09:55:30 +0700

    First Assignment with Python

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+MyName = "Steven Lee"
+MyAgeInYears = 15.83
+MyHeightInMeters = 1.67
+SquareLengthInFeet = 3
+RectangleLengthInFeet = 2
+RectangleHeightInFeet = 4
+MyAgeInMonths = MyAgeInYears*12
+MyNumberOfYearsLeftToLive = 81.37 - MyAgeInYears
+MyHeightInFeet = 3.28084*MyHeightInMeters
+MyHeightDifferenceFromKoreanAverageInMeters = 175.26 - MyHeightInMeters
+SquareAreaInFeet = SquareLengthInFeet^2
+HalfTheVolumeofCubeWithSquareLengthInFeetCubed = SquareLengthInFeet^3/2.0
+OneNinthTheAreaOfRectangleInFeet = RectangleLengthInFeet*RectangleHeightInFeet/9
+print "Hello, my name is", str(MyName), "and I am", str(MyAgeInYears), "years old."
