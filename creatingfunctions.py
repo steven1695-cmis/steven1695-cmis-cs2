f083bebe8200e2679cab0da59f9dd6ed8f0301ad
cmis <cmis@cmis-hs02.(none)>
2016-02-05 09:55:31 +0700

    Making functions

---
+++ creatingfunctions.py
@@ -0,0 +1,27 @@
+def Minion_one():
+	print "Minion1: HAHAHA! Got you now you sneaky runt!"
+	print "That'll teach you to steal from US!"
+	print " "
+
+def Minion_two():
+	print "Minion2: Yeah, yeah! That'll teach you!"
+	print " "
+
+def Boss():
+	print "Boss: SILENCE! YOU FOOLISH MORTALS! DO NOT SPEAK WITH SUCH INSOLENCE IN MY PRESENCE!"
+	print " "
+
+def Minion_three():
+	print "Minion3: ehh... sorry boss, we just trying to intimidate the runt."
+	print " "
+
+def Protagonist(name):
+	print name+":"+" It is I, " + name + "."
+
+def WonderBoy(a, b, c)
+	print "He's too powerful!!"
+Minion_one()
+Minion_two()
+Boss()
+Minion_three()
+Protagonist("Link")
