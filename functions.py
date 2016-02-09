9e247fdaa69fc79503abcaffbab51969b2d3141f
cmis <cmis@cmis-hs02.(none)>
2016-02-09 14:05:27 +0700

    Moe Work

--- functions.py
+++ functions.py
@@ -1,14 +1,43 @@
 def add(a, b):
 	return a+b
 
+print add(3, 4)
+print add(32, 23)
 def sub(a, b):
 	return a-b
 
+print sub(23, 3)
+print sub(5, 43)
 def mul(a, b):
 	return a*b
+
 
 def div(a, b):
 	return a/b
 
 def hours_from_seconds(a):
 	return a/3600
+
+import math
+
+def circle_area(a):
+	return math.pi*(a**2)
+
+def sphere_volume(a):
+	return math.pi*(a**3)*4/3
+
+print sphere_volume(5)
+def avg_volume(a,b):
+	return (math.pi*(a**3)*(4/3) + math.pi*(b**3)*(4/3))/2
+
+
+def area(a, b, c):
+	return s = (a+b+c)/2
+	return math.sqrt(s(s-a)(s-b)(s-c))
+
+def right_align(a):
+	
+def center(a):
+	
+def msg_box(a):
+	
