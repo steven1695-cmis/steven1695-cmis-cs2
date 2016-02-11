865d67586202fd9917c4fb7252ad0acebf7bf71d
cmis <cmis@cmis-hs02.(none)>
2016-02-11 09:55:37 +0700

    Almost done

--- functions.py
+++ functions.py
@@ -11,33 +11,57 @@ print sub(5, 43)
 def mul(a, b):
 	return a*b
 
-
+print mul(4, 2)
+print mul(54, 2)
 def div(a, b):
 	return a/b
 
+print div(10,2)
+print div(9,8.0)
 def hours_from_seconds(a):
-	return a/3600
+	return a/3600.0
 
+print hours_from_seconds(86400), "hours"
+print hours_from_seconds(7200), "hours"
 import math
 
 def circle_area(a):
 	return math.pi*(a**2)
 
+print circle_area(5)
+print circle_area(2.5)
 def sphere_volume(a):
 	return math.pi*(a**3)*4/3
 
 print sphere_volume(5)
-def avg_volume(a,b):
-	return (math.pi*(a**3)*(4/3) + math.pi*(b**3)*(4/3))/2
+print sphere_volume(7)
+def avg_volume(a,b): 
+	j = a/2
+	k = b/2
+	s = math.pi*(j**3)*4/3 + math.pi*(k**3)*4/3
+	d = s/2.0
+	return d
 
-
+print avg_volume(10, 20)
+print avg_volume(3, 5)
 def area(a, b, c):
-	return s = (a+b+c)/2
-	return math.sqrt(s(s-a)(s-b)(s-c))
+	s = (a+b+c)/2.0
+	j = (s-a)
+	k = (s-b)
+	l = (s-c)
+	t = s*j*k*l
+	d = math.sqrt(t)
+	return d
 
+print area(1, 2, 2.5)
+print area(5, 12, 13)
 def right_align(a):
-	
+	return a.rjust(80)
+
+print right_align("Hello")
+print right_align("I'm depressed")
 def center(a):
-	
-def msg_box(a):
-	
+	return a.center(80)
+
+print center("Hello")
+print center("Cheese")
