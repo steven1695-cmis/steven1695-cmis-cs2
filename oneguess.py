035ed99d76fcc59775468a5c65508986638e2b27
cmis <cmis@cmis-hs02.(none)>
2016-03-15 09:43:24 +0700

    Finished guess assignment

--- oneguess.py
+++ oneguess.py
@@ -1,18 +1,35 @@
 import random
 
-main():
-	minimum = float(raw_input("What is the minimum number? "))
-	maximum = float(raw_input("What is the maximum number? "))
+# Defintion of Functions
+def output(target, guess, difference):
+	print """
+The target was {}.
+Your guess was {}.""".format(target, guess)
+	if target > guess:
+		print "Thats under by {}.".format(difference)
+	elif target < guess:
+		print "Thats over by {}.".format(difference)
+	elif target == guess:
+		print "Correct! You must be a psychic!"
 	
-	print """I'm thinking of a number from""" + minimum + """to""" + maximum + """."""
 
-	guess = float(raw_input("What do you think it is?: "))
-	answer = random.randint(minimum, maximum)
-	output ="""
-	The target was {}.
-	Your guess was {}.
-	That's under by {}."""
+def main():
+	#Input
+	minimum = raw_input("What is the minimum number? ")
+	maximum = raw_input("What is the maximum number? ")
+	
 	#Output
+	print """I'm thinking of a number from """ + minimum + """ to """ + maximum + """."""
+
+	#Input 
+	guess = int(raw_input("What do you think it is?: "))
+	
+	#Processing
+	answer = random.randint(int(minimum), int(maximum))
+	out = output(answer, guess, abs(guess - answer))
+
+	#Output
+	return out
 	
 
 
