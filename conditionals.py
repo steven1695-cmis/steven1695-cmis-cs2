7b0e826793af8071d93b6b3828c9a262319c1826
cmis <cmis@cmis-hs09.(none)>
2016-03-17 14:35:51 +0700

    More work on the conditionals assignment.

--- conditionals.py
+++ conditionals.py
@@ -17,26 +17,75 @@ print "Stealing the Artifact of Dumbo"
 #If you have passed the following obstacles, you are now in front of the Artifact of Dumbo but there are two. One is a fake.
 #You pick one out on random and leave the museum with the artifact.
 
+#Defined Functions used in main
+import random
+def enter():
+	EnterChoice = raw_input("You make a choice of how to enter. (front door/ back door): ")
+
+	if EnterChoice == "front door":
+		print "You enter through the front door. You run into two guards on night duty."
+		guardchoice = raw_input("Will you engage the guards or run away? (fight guards/ run away): ")
+		if guardchoice == "fight guards":
+			weaponchoice = yourweapon()
+			guardfight(weaponchoice)
+		elif guardchoice == "run away":
+			print "You attempted to flee but you were cornered by the guards and arrested"
+		else:
+			print "That is not a viable choice. Try again."
+	elif EnterChoice == "back door":
+		print "You enter through the back door. There are no guards. However, you are met by red sensor beams on the left and a karate rabbit on the right."
+		choice = raw_input("Which side do you want to go to? (left/ right): ")
+		if choice == "left":
+			sensorbeams()
+		elif choice == "right":
+			karatebunny()
+		else:
+			print "Sorry, not an option. Try again."
+	else:
+		print "You were unable to enter through the method of choice. Try again."
+def karatebunny():
+def sensorbeams():
+def yourweapon():
+	pocket = raw_input("You chose to engage the two guards, you need a weapon. Which pocket will you put your hand into to find a weapon? (left/ right): ")
+	if pocket == "left":
+		leftweapon = raw_input("You rummage your left pocket to find three items. Which one do you pick as your weapon? (paper clip/ apple/ gun): ")
+		if leftweapon == "paper clip":
+			strength = random.randint(0, 10)
+			print "Your paper clip was a magical sword in disguise. It changes it's strength depending on its mood. Its power level today is " + str(strength) + "."
+			return strength
+		elif leftweapon == "apple":
+			print "How do you expect to fight with an apple? Well... if you insist..."
+			return 1
+		elif leftweapon == "gun":
+			print "Wise choice, a gun should easily handle two guards."
+			return 10
+		else:
+			print "Sorry, not an option. Try again."
+
+	elif pocket == "right":
+		rightweapon = raw_input("You rummage your right pocket to find two items. Which one do you pick as your weapon? (dagger/ cheesestick): ")
+		if rightweapon == "dagger":
+			print "The dagger was actually made of rubber. It is harmless."
+			return 0
+		elif rightweapon == "cheesestick":
+			print "For real man? A cheesestick as a weapon?"
+			return 2
+		else:
+			print "Sorry, not an option. Try again."
+	else:
+		print "Are you sure thats how you write left or right?"
 
 def guardfight(yourweapon):
 	baton = 5
 	if yourweapon > baton:
+		print "Your weapon of choice overpowers that of the guards."
 		print "You win the fight with the guards"
 	else:
-		print """You are arrested by the guards: Game Over"""
+		print """Your poor choice in a weapon ended up with your loss. You are arrested by the guards: Game Over"""
 
+#Main
 def main():
 	print "You are a robber and you are attempting to steal the Artifact of Dumbo. It is now night time and you decide on how to enter the museum which holds this sacred object."
-
-	EnterChoice = raw_input("You make a choice of how to enter. (Front Door/ Back Door): ")
-
-	if EnterChoice == "Front Door":
-		print "You enter through the front door. You run into two guards on night duty."
-	elif EnterChoice == "Back Door":
-		print "You enter through the back door. There are no guards. However, you are met by red sensor beams on the left and a karate rabbit on the right."
-	else:
-		print "You were unable to enter through the method of choice. Try again."
-		
-	
+	enter()
 
 main()
