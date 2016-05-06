031774ed98060950d1edccbf518de83f2bf8e649
cmis <cmis@cmis-hs11.(none)>
2016-05-06 11:41:53 +0700

    I DID IT

--- newguess.py
+++ newguess.py
@@ -1,16 +1,22 @@
 #You will have three rounds to properly guess a number between 0 and 100. Each round gives you 5 chances to find the numbe
 import random
 
-def roundnumber(number):
-	print "Round" + str(number)
-	roundnumber(number+1)
+def roundnumber(times):
+	if times > 3:
+		pass
+	else:
+		print "Round" + str(times)
+		guess(8, 0)
+		return roundnumber(times+1)
+		
 
 def guess(run, ans):
 	if run == 0:
 		print """Your not very good at this
 """
-	elif run == 5:
+	elif run == 8:
 		ans = random.randint(0, 100)
+		guess(run-1, ans)
 	else:
 		inp = int(raw_input("Your guess: "))
 		if inp > ans:
@@ -24,8 +30,6 @@ def guess(run, ans):
 """
 
 def main():
-	guess(5,0)
-	guess(5,0)
-	guess(5,0)
+	roundnumber(1)
 
 main()
