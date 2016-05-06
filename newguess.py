e0f1a789bf0ba10311e4d04c7349f32c79f7a8d3
cmis <cmis@cmis-hs11.(none)>
2016-05-06 11:55:38 +0700

    Doesn't Work

--- newguess.py
+++ newguess.py
@@ -6,30 +6,32 @@ def roundnumber(times):
 		pass
 	else:
 		print "Round" + str(times)
-		guess(8, 0)
+		guess(8, 0, 0)
 		return roundnumber(times+1)
 		
 
-def guess(run, ans):
+def guess(run, ans, correct):
 	if run == 0:
 		print """Your not very good at this
 """
 	elif run == 8:
 		ans = random.randint(0, 100)
-		guess(run-1, ans)
+		guess(run-1, ans, correct)
 	else:
 		inp = int(raw_input("Your guess: "))
 		if inp > ans:
 			print "Too high"
-			guess(run-1, ans)
+			guess(run-1, ans, correct)
 		elif inp < ans:
 			print "Too low"
-			guess(run-1, ans)
+			guess(run-1, ans, correct)
 		else:
 			print """Correct
 """
+			guess(run-1, ans, correct+1)
 
 def main():
 	roundnumber(1)
+	print "You were correct " + str(correct) + " times."
 
 main()
