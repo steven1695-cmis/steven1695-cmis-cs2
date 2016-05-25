4a893c2fbe3a42cf48087a58a11736db0ee18437
cmis <cmis@cmis-hs11.(none)>
2016-05-25 11:04:21 +0700

    yeah

--- cs2quiz3.py
+++ cs2quiz3.py
@@ -62,7 +62,49 @@ def question(count, total):
 def main():
 	question(0, 0)
 
-main()
+#main()
+
+def function1(x, n):
+	if n <= 0:
+		return x
+	else:
+		return 1 + function1(x, n-1)
+
+def function2(a, b):
+	if a <= 0:
+		return 1 + function2(a+1, b**a)
+	else:
+		return b
+
+def function3(a, b):
+	if b <= 0:
+		return a
+	else:
+		return a + b + function3(a, b - 1)
+
+def function4(a, b, c):
+	if a > b and a > c:
+		return 1 + function4(b + 1, c, a)
+	elif b > a or b > c:
+		return 1 - function4(b - 1, c, a)
+	else:
+		return a + b + c
+
+print function1(2, 6)
+print function1(6, 2)
+print function1(-1, -1)
+
+print function2(2, 2)
+print function2(0, 0)
+print function2(-2, -2)
+
+print function3(-2, 3)
+print function3(4, -2)
+print function3(5, 5)
+
+print function4(1,2,3)
+print function4(3,2,1)
+print function4(1,3,2)
 
 
 
